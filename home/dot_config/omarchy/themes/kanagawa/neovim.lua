8cb7a8fcaadab3d1138d3bc657a9ba3e35f5662f
Zillordo <konecnyallan@gmail.com>
2025-08-24 17:53:57 +0200

    omarchy themes

---
+++ home/dot_config/omarchy/themes/kanagawa/neovim.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"rebelot/kanagawa.nvim",
+		opts = {
+			transparent = true,
+			terminalcolors = true,
+			colors = {
+				theme = {
+					all = {
+						ui = {
+							bg_gutter = "none",
+						},
+					},
+				},
+			},
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "kanagawa",
+		},
+	},
+}
